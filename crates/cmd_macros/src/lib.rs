04d6a279293e10433104db8575fdde87444b8704
Alex Helfet <alex.helfet@gmail.com>
2023-04-02 12:49:38 +0100

    Remove bracket requirements when using pipes or redirects.

--- crates/cmd_macros/src/lib.rs
+++ crates/cmd_macros/src/lib.rs
@@ -3,80 +3,143 @@ mod lazy_regex;
 
 #[macro_export]
 macro_rules! cmd {
+    // In the external interface $bin is matched against ident or literal.
+    // It might seem like we could match $bin:tt instead, because ( @bin $bin )
+    // will check the token type, but then $bin:tt will match all the internal
+    // patterns with @-prefixes.
+
+    ( $bin:ident $( $rest:tt )* ) => {
+        $crate::cmd!( @cmd_muncher ( ( $crate::cmd!( @bin $bin ) ) )
+                                   ( $( $rest )* ) )
+    };
+
+    ( $bin:literal $( $rest:tt )* ) => {
+        $crate::cmd!( @cmd_muncher ( ( $crate::cmd!( @bin $bin ) ) )
+                                   ( $( $rest )* ) )
+    };
+
+    (@cmd_muncher ( $( $wip:tt )+ ) ( ) ) => {
+        $crate::cmd!( @dcmd $( $wip )+ )
+    };
+
+    (@cmd_muncher ( $( $wip:tt )+ ) ( | $( $rest:tt )+ ) ) => {
+        $crate::cmd!( @dcmd $( $wip )+ )
+            .pipe( $crate::cmd!( $( $rest )+ ) )
+    };
+
+    (@cmd_muncher ( $( $wip:tt )+ ) ( < $( $rest:tt )+ ) ) => {
+        $crate::cmd!( @opt_muncher ( $crate::cmd!( @dcmd $( $wip )+ ) )
+                                   ( < $( $rest )* )
+        )
+    };
+
+    (@cmd_muncher ( $( $wip:tt )+ ) ( > $( $rest:tt )+ ) ) => {
+        $crate::cmd!( @opt_muncher ( $crate::cmd!( @dcmd $( $wip )+ ) )
+                                   ( > $( $rest )* )
+        )
+    };
+
+    (@cmd_muncher ( $( $wip:tt )+ ) ( <<< $( $rest:tt )+ ) ) => {
+        $crate::cmd!( @opt_muncher ( $crate::cmd!( @dcmd $( $wip )+ ) )
+                                   ( <<< $( $rest )* )
+        )
+    };
+
+    (@cmd_muncher ( $( $wip:tt )+ ) ( $arg:tt $( $rest:tt )* ) ) => {
+        $crate::cmd!( @cmd_muncher ( $( $wip )+
+                                     ( $crate::cmd!( @arg $arg ) )
+                                   )
+                                   ( $( $rest )* )
+        )
+    };
+
+    // ( @opt_muncher ( $wip:expr ) ( $( $opts:tt )* ) ) -> expr<duct::Expression>
+
+    (@opt_muncher ( $wip:expr ) ( ) ) => {
+        $wip
+    };
+
+    (@opt_muncher ( $wip:expr ) ( < null $( $rest:tt )* ) ) => {
+        $crate::cmd!( @opt_muncher (
+                          $wip.stdin_null()
+                      )
+                      ( $( $rest )* )
+        )
+    };
+
+    (@opt_muncher ( $wip:expr ) ( < $path:tt $( $rest:tt )* ) ) => {
+        $crate::cmd!( @opt_muncher (
+                          $wip.stdin_path( $crate::cmd!( @path $path ) )
+                      )
+                      ( $( $rest )* )
+        )
+    };
+
+    (@opt_muncher ( $wip:expr ) ( > null $( $rest:tt )* ) ) => {
+        $crate::cmd!( @opt_muncher (
+                          $wip.stdout_null()
+                      )
+                      ( $( $rest )* )
+        )
+    };
+
+    (@opt_muncher ( $wip:expr ) ( > $path:tt $( $rest:tt )* ) ) => {
+        $crate::cmd!( @opt_muncher (
+                          $wip.stdout_path( $crate::cmd!( @path $path ) )
+                      )
+                      ( $( $rest )* )
+        )
+    };
+
+    (@opt_muncher ( $wip:expr ) ( <<< $in:ident $( $rest:tt )* ) ) => {
+        $crate::cmd!( @opt_muncher (
+                          $wip.stdin_bytes( $in )
+                      )
+                      ( $( $rest )* )
+        )
+    };
+
+    (@opt_muncher ( $wip:expr ) ( | $( $rest:tt )+ ) ) => {
+        $wip.pipe( $crate::cmd!( $( $rest )+ ) )
+    };
+
+    (@dcmd $bin:tt ) => {
+        ::duct::cmd( $bin, [] as [&'static str; 0] )
+    };
+
+    (@dcmd $bin:tt $( $arg:tt )+ ) => {
+        ::duct::cmd( $bin, [ $( $arg ),* ] )
+    };
+
+    (@arg $arg:ident) => {
+        $crate::cmd!( @word $arg )
+    };
+
+    (@arg $arg:literal) => {
+        $crate::cmd!( @word $arg )
+    };
+
+    (@path $path:ident) => {
+        $crate::cmd!( @word $path )
+    };
+
+    (@path $path:literal) => {
+        $crate::cmd!( @word $path )
+    };
+
+    (@bin $bin:ident) => {
+        $crate::cmd!( @word $bin )
+    };
+
+    (@bin $bin:literal) => {
+        $crate::cmd!( @word $bin )
+    };
+
     (@word $id:ident) => {
         stringify!($id)
     };
 
     (@word $lit:literal) => { $lit };
-
-    (@dcmd $bin:tt ) => {
-        ::duct::cmd( $crate::cmd!(@word $bin), [] as [&'static str; 0] )
-    };
-
-    (@dcmd $bin:tt $( $arg:tt )+ ) => {
-        ::duct::cmd( $crate::cmd!(@word $bin), [ $( $crate::cmd!(@word $arg) ),* ] )
-    };
-
-    (@out_redirect $out:tt ) => {
-        $crate::cmd!( @word $out )
-    };
-
-    (@in_redirect $in:tt ) => {
-        $crate::cmd!( @word $in )
-    };
-
-    (@redirect $expr:ident) => { $expr };
-
-    (@redirect $expr:ident | $( $right:tt )+ ) => {
-        $expr.pipe( $crate::cmd!( $( $right )+ ) )
-    };
-
-    (@redirect $expr:ident <<< $var:ident $( $rest:tt )* ) => {{
-        let e = $expr.stdin_bytes($var);
-        $crate::cmd!( @redirect e $( $rest )* )
-    }};
-
-    (@redirect $expr:ident < null $( $rest:tt )* ) => {{
-        let e = $expr.stdin_null();
-        $crate::cmd!( @redirect e $( $rest )* )
-    }};
-
-    (@redirect $expr:ident < $in:tt $( $rest:tt )* ) => {{
-        let e = $expr.stdin_path( $crate::cmd!( @in_redirect $in ) );
-        $crate::cmd!( @redirect e $( $rest )* )
-    }};
-
-    (@redirect $expr:ident > null $( $rest:tt )* ) => {{
-        let e = $expr.stdout_null();
-        $crate::cmd!( @redirect e $( $rest )* )
-    }};
-
-    (@redirect $expr:ident > $out:tt $( $rest:tt )* ) => {{
-        let e = $expr.stdout_path( $crate::cmd!( @out_redirect $out ) );
-        $crate::cmd!( @redirect e $( $rest )* )
-    }};
-
-    ( ( $( $left:tt )+ ) | $( $right:tt )+ ) => {
-        $crate::cmd!( @dcmd $( $left )+ )
-            .pipe( $crate::cmd!( $( $right )+ ) )
-    };
-
-    ( ( $( $left:tt )+ ) $( $redirect:tt )+ ) => {{
-        let e = $crate::cmd!( @dcmd $( $left )+ );
-        $crate::cmd!( @redirect e $( $redirect )+ )
-    }};
-
-    ( ( $( $left:tt )+ ) ) => {
-        $crate::cmd!( @dcmd $( $left )+ )
-    };
-
-    ( $bin:ident $( $args:tt )* ) => {
-        $crate::cmd!( @dcmd $bin $( $args )* )
-    };
-
-    ( $bin:literal $( $args:tt )* ) => {
-        $crate::cmd!( @dcmd $bin $( $args )* )
-    };
 }
 
 #[cfg(test)]
@@ -100,18 +163,13 @@ mod tests {
         let _expr: duct::Expression = cmd!("bin" arg1);
         let _expr: duct::Expression = cmd!("bin" "arg1");
 
-        case(cmd!((bin)),
-r#"
-Cmd(["bin"])
-"#);
-
-        case(cmd!((bin1 arg11) | bin2 arg21),
+        case(cmd!(bin1 arg11 | bin2 arg21),
 r#"
 Pipe(Cmd(["bin1", "arg11"]), 
      Cmd(["bin2", "arg21"])
 )"#);
 
-        case(cmd!((bin1 arg11) | (bin2 arg21) | bin3 arg31),
+        case(cmd!(bin1 arg11 | bin2 arg21 | bin3 arg31),
 r#"
 Pipe(Cmd(["bin1", "arg11"]), 
      Pipe(Cmd(["bin2", "arg21"]), 
@@ -120,43 +178,43 @@ Pipe(Cmd(["bin1", "arg11"]),
 )"#);
 
 
-        case(cmd!((bin arg1) > file),
+        case(cmd!(bin arg1 > file),
 r#"
 Io(StdoutPath("file"), 
    Cmd(["bin", "arg1"])
 )"#);
 
-        case(cmd!((bin arg1) > null),
+        case(cmd!(bin arg1 > null),
 r#"
 Io(StdoutNull, 
    Cmd(["bin", "arg1"])
 )"#);
 
-        case(cmd!((bin arg1) > "null"),
+        case(cmd!(bin arg1 > "null"),
 r#"
 Io(StdoutPath("null"), 
    Cmd(["bin", "arg1"])
 )"#);
 
-        case(cmd!((bin arg1) < file),
+        case(cmd!(bin arg1 < file),
 r#"
 Io(StdinPath("file"), 
    Cmd(["bin", "arg1"])
 )"#);
 
-        case(cmd!((bin arg1) < "null"),
+        case(cmd!(bin arg1 < "null"),
 r#"
 Io(StdinPath("null"), 
    Cmd(["bin", "arg1"])
 )"#);
 
-        case(cmd!((bin arg1) < null),
+        case(cmd!(bin arg1 < null),
 r#"
 Io(StdinNull, 
    Cmd(["bin", "arg1"])
 )"#);
 
-        case(cmd!((bin arg1) < file_in > file_out),
+        case(cmd!(bin arg1 < file_in > file_out),
 r#"
 Io(StdoutPath("file_out"), 
    Io(StdinPath("file_in"), 
@@ -164,7 +222,7 @@ Io(StdoutPath("file_out"),
    )
 )"#);
 
-        case(cmd!((bin arg1) > file_out < file_in),
+        case(cmd!(bin arg1 > file_out < file_in),
 r#"
 Io(StdinPath("file_in"), 
    Io(StdoutPath("file_out"), 
@@ -173,7 +231,7 @@ Io(StdinPath("file_in"),
 )"#);
 
 
-        case(cmd!((bin1 arg1) < file_in | bin2),
+        case(cmd!(bin1 arg1 < file_in | bin2),
 r#"
 Pipe(Io(StdinPath("file_in"), 
         Cmd(["bin1", "arg1"])
@@ -183,7 +241,7 @@ Pipe(Io(StdinPath("file_in"),
 
         let in_bytes = "abc";
 
-        case(cmd!((bin arg1) <<< in_bytes),
+        case(cmd!(bin arg1 <<< in_bytes),
 r#"
 Io(StdinBytes([97, 98, 99]), 
    Cmd(["bin", "arg1"])
